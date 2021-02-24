3773f866862dbe22b382a9c92b220d661797bdb7
Dani <danikav1989@gmail.com>
2021-02-24 15:13:59 +0000

    test 15 added

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -21,11 +21,13 @@ def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
 
 def number_to_full_month_name(number_1):
-    months = ["Unknown","January","Febuary","March","April","May","June",
+    months = ["January","Febuary","March","April","May","June",
     "July","August","September","October","November","December"]
-    return months [number_1]
+    return months [number_1 - 1]
 
 def number_to_short_month_name(number_1):
-    short_months = ["Unknown","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
-    return short_months [number_1]
-    
+    short_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    return short_months [number_1 - 1 ]
+
+def vol_of_cube(side_length):
+    return side_length **3
